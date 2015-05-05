0d26bedda61f2b412344ad62666ff066d3652b11
ZHONGJIE ZHANG <zhongjiezhangMac@192.168.2.101>
2015-05-05 21:28:53 +0800

    update

--- app/views/admin/roles/create.blade.php
+++ app/views/admin/roles/create.blade.php
@@ -30,7 +30,6 @@
         <div class="form-group">
         	@foreach ($permissions as $permission)
 			<label>
-				
 				<input type="checkbox" id="permissions[{{{ $permission['id'] }}}]" name="permissions[{{{ $permission['id'] }}}]" value="1"{{{ (isset($permission['checked']) && $permission['checked'] == true ? ' checked="checked"' : '')}}} />
 				{{{ $permission['display_name'] }}}
 			</label>
@@ -182,9 +181,13 @@
 					$("#role_create").attr("disabled","disabled");
 					$("#role_mode_val").val("edit");
 					$("#role_id").val(id);
-					$("input").prop("checked", false);
+					$("input[type='checkbox']").prop("checked", false);
 					for(var i = 0; i < result['permissionIds'].length; ++i){
-						$("#permissions[" + result['permissionIds'][i] + "]").prop('checked',true);
+						//$("#permissions[" + result['permissionIds'][i] + "]").click()
+						var p = $("#permissions[" + result['permissionIds'][i] + "]");
+						p.prop("disabled", true);
+						alert(($("#permissions[89]").prop('type')));
+						$.uniform.update(p);
 					}
 					$.uniform.update();
 				}
