d272cee8669e3b09db153d08543f1e1af919c154
cnicq <swjtuboy@163.com>
2015-05-15 18:42:28 +0800

    -

--- app/views/mwork/manage/company.blade.php
+++ app/views/mwork/manage/company.blade.php
@@ -1,238 +1,419 @@
 @extends('mwork.layouts.default')
-
+@section('csss')
+<link rel="stylesheet" type="text/css" href="/bootstrap2/media/css/datepicker.css" />
+@stop
 @section('content')
-    <div class="portlet box green">
+<div class="tabbable tabbable-custom tabbable-full-width">
+				<ul class="nav nav-tabs">
 
-	<div class="portlet-title">
+						<li class="active"><a href="#tab_1_1" data-toggle="tab">公司列表</a></li>
 
-		<div class="caption"><i class="icon-reorder"></i>公司管理</div>
+						<li><a href="#tab_1_2" data-toggle="tab">新增公司</a></li>
 
-		<div class="tools">
+				</ul>
 
-			<a href="javascript:;" class="collapse"></a>
+					<div class="tab-content">
 
-			<a href="#portlet-config" data-toggle="modal" class="config"></a>
+						<div class="tab-pane row-fluid active" id="tab_1_1">
 
-			<a href="javascript:;" class="reload"></a>
+							<!-- BEGIN FORM : company list-->
 
-			<a href="javascript:;" class="remove"></a>
+							<div class="portlet-body">
 
-		</div>
+								<table class="table table-striped table-bordered table-hover table-full-width" id="sample_1">
+									<thead>
+										<tr>
+											<th>中文名</th>
+											<th>英文名</th>
+											<th>城市</th>
+											<th>详细地址</th>
+											<th>行业</th>
+										</tr>
+									</thead>
+									<tbody>
 
+										@foreach ($companys as $company)
+										<tr>
+											<td>{{$company['chinesename']}}</td>
+											<td>{{$company['englishname']}}</td>
+											<td>{{$company['city']}}</td>
+											<td>{{$company['location']}}</td>
+											<td>{{$company['industry']}}</td>
+										</tr>
+
+										@endforeach
+										
+									</tfoot>
+
+									</tbody>
+								</table>
+								<ul class="pagination">
+								{{$companys->links()}}
+								</ul>
+								</div>
+
+							<!-- END FORM : company list-->   
+							
+						</div>
+		
+						<div class="tab-pane row-fluid" id="tab_1_2">
+
+							<!-- BEGIN FORM : company manange-->
+
+							<form action="#" class="form-horizontal">
+								<h3 class="form-section">公司信息</h3>
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">中文名称</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="阿里巴巴">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">English Name</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="Alibaba">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">所在城市</label>
+
+											<div class="controls">
+
+												<select class="m-wrap span12">
+
+													<option value="1">上海</option>
+													<option value="2">北京</option>
+													<option value="3">广州</option>
+													<option value="4">深圳</option>
+
+												</select>
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">详细地址</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="上海...">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->        
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">行业</label>
+
+											<div class="controls">
+
+												<select class="m-wrap span12">
+
+													<option value="1">互联网/移动互联网</option>
+													<option value="2">游戏</option>
+													<option value="3">传统IT</option>
+													<option value="4">金融互联网</option>
+
+												</select>
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->
+
+								<h3 class="form-section">联系人信息</h3>
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">中文名称</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="张三">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">English Name</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="Justin Zhang">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">移动电话</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">固定电话</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->        
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">Email</label>
+
+											<div class="controls">
+
+												<div class="input-prepend"><span class="add-on">@</span><input class="m-wrap " type="text" placeholder="Email Address">
+
+												</div>
+
+											</div>
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">QQ</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="">
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->
+
+								<h3 class="form-section">合同信息</h3>
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">开始时间</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="张三">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">截止时间</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="Justin Zhang">
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<!--/span-->
+
+								</div>
+
+								<!--/row-->
+
+								<div class="row-fluid">
+
+									<div class="span6 ">
+
+										<div class="control-group">
+
+											<label class="control-label">附件上传</label>
+
+											<div class="controls">
+
+												<input type="text" class="m-wrap span12" placeholder="">
+
+											</div>
+
+										</div>
+
+									</div>
+
+
+
+								</div>
+
+								<!--/row-->        
+
+								<div class="form-actions">
+
+									<button type="submit" class="btn blue"><i class="icon-ok"></i> 保存</button>
+
+									<button type="button" class="btn">清除</button>
+
+								</div>
+
+							</form>
+
+							<!-- END FORM : company manage-->   
+						</div>
+					</div>
+
+			</div>
 	</div>
+@stop
 
+@section("scripts")
 
-	<div class="portlet-body form">
+<script type="text/javascript" src="/bootstrap2/media/js/form-components.js"></script>
+<script type="text/javascript" src="/bootstrap2/media/js/bootstrap-datepicker.js"></script>
 
-		<!-- BEGIN FORM : company manange-->
+<script>
 
-		<form action="#" class="form-horizontal">
-			<h3 class="form-section">新增</h3>
+		jQuery(document).ready(function() {       
 
-			<div class="row-fluid">
+		   if (jQuery().datepicker) {
+            $('.date-picker').datepicker({
+                rtl : App.isRTL()
+            });
+        }
 
-				<div class="span6 ">
+		});
 
-					<div class="control-group">
-
-						<label class="control-label">中文名称</label>
-
-						<div class="controls">
-
-							<input type="text" class="m-wrap span12" placeholder="阿里巴巴">
-
-						</div>
-
-					</div>
-
-				</div>
-
-				<!--/span-->
-
-				<div class="span6 ">
-
-					<div class="control-group">
-
-						<label class="control-label">English Name</label>
-
-						<div class="controls">
-
-							<input type="text" class="m-wrap span12" placeholder="Alibaba">
-
-						</div>
-
-					</div>
-
-				</div>
-
-				<!--/span-->
-
-			</div>
-
-			<!--/row-->
-
-			<div class="row-fluid">
-
-				<div class="span6 ">
-
-					<div class="control-group">
-
-						<label class="control-label">所在城市</label>
-
-						<div class="controls">
-
-							<select class="m-wrap span12">
-
-								<option value="1">上海</option>
-								<option value="2">北京</option>
-								<option value="3">广州</option>
-								<option value="4">深圳</option>
-
-							</select>
-
-						</div>
-
-					</div>
-
-				</div>
-
-				<!--/span-->
-
-				<div class="span6 ">
-
-					<div class="control-group">
-
-						<label class="control-label">详细地址</label>
-
-						<div class="controls">
-
-							<input type="text" class="m-wrap span12" placeholder="上海...">
-
-						</div>
-
-					</div>
-
-				</div>
-
-				<!--/span-->
-
-			</div>
-
-			<!--/row-->        
-
-			<div class="row-fluid">
-
-				<div class="span6 ">
-
-					<div class="control-group">
-
-						<label class="control-label">行业</label>
-
-						<div class="controls">
-
-							<select class="m-wrap span12">
-
-								<option value="1">互联网/移动互联网</option>
-								<option value="2">游戏</option>
-								<option value="3">传统IT</option>
-								<option value="4">金融互联网</option>
-
-							</select>
-						</div>
-
-					</div>
-
-				</div>
-
-				<!--/span-->
-
-				<div class="span6 ">
-
-					<div class="control-group">
-
-						<label class="control-label">是否有合同</label>
-
-						<div class="controls">                                                
-
-							<label class="radio">
-
-							<div class="radio"><span><input type="radio" name="optionsRadios2" value="option1"></span></div>
-
-							是
-
-							</label>
-
-							<label class="radio">
-
-							<div class="radio"><span class="checked"><input type="radio" name="optionsRadios2" value="option2" checked=""></span></div>
-
-							否
-
-							</label>  
-
-						</div>
-
-					</div>
-
-				</div>
-
-				<!--/span-->
-
-			</div>
-
-			<!--/row-->
-
-			<div class="form-actions">
-
-				<button type="submit" class="btn blue"><i class="icon-ok"></i> 保存</button>
-
-				<button type="button" class="btn">清除</button>
-
-			</div>
-
-		</form>
-
-		<!-- END FORM : company manage-->                
-
-		<!-- BEGIN FORM : company list-->
-
-		<div class="portlet-body">
-
-			<table class="table table-striped table-bordered table-hover table-full-width" id="sample_1">
-				<thead>
-					<tr>
-						<th>中文名</th>
-						<th>英文名</th>
-						<th>城市</th>
-						<th>详细地址</th>
-						<th>行业</th>
-						<th>是否有合同</th>
-					</tr>
-				</thead>
-				<tbody>
-
-					@foreach ($companys as $company)
-					<tr>
-						<td>{{$company['chinesename']}}</td>
-						<td>{{$company['englishname']}}</td>
-						<td>{{$company['city']}}</td>
-						<td>{{$company['location']}}</td>
-						<td>{{$company['industry']}}</td>
-						<td>{{$company['contract']}}</td>
-					</tr>
-
-					@endforeach
-					
-				</tfoot>
-
-				</tbody>
-			</table>
-			<ul class="pagination">
-			{{$companys->links()}}
-			</ul>
-			</div>
-
-		<!-- END FORM : company list-->   
-	</div>
-</div>
+	</script>
 @stop
