f564574aaf4ebb8334d416786ca2226fb4b41e46
cnicq <swjtuboy@163.com>
2015-06-05 19:53:13 +0800

    - work

--- app/views/mwork/layouts/modal.blade.php
+++ app/views/mwork/layouts/modal.blade.php
@@ -12,16 +12,13 @@
 
 	<meta charset="utf-8" />
 
-	<title>Mapping</title>
+	<title>Metronic | Layouts - Horzontal Menu 1</title>
 
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
 	<meta content="" name="description" />
 
 	<meta content="" name="author" />
-	<meta name="csrf-token" content="<?= csrf_token() ?>">
-
-	@yield('metas')
 
 	<!-- BEGIN GLOBAL MANDATORY STYLES -->
 
@@ -43,66 +40,7 @@
 
 	<!-- END GLOBAL MANDATORY STYLES -->
 
-	<!-- BEGIN PAGE LEVEL STYLES -->
-
-	<link rel="stylesheet" type="text/css" href="/bootstrap2/media/css/select2_metro.css" />
-
-	<link rel="stylesheet" href="/bootstrap2/media/css/DT_bootstrap.css" />
-
-	<!-- END PAGE LEVEL STYLES -->
-
 	<link rel="shortcut icon" href="/bootstrap2/media/image/favicon.ico" />
-
-	@yield('csss')
-
-	<!-- BEGIN CORE PLUGINS -->
-
-	<script src="/bootstrap2/media/js/jquery-1.10.1.js" type="text/javascript"></script>
-
-	<script src="/bootstrap2/media/js/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
-
-	<!-- IMPORTANT! Load jquery-ui-1.10.1.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-
-	<script src="/bootstrap2/media/js/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>      
-
-	<script src="/bootstrap2/media/js/bootstrap.min.js" type="text/javascript"></script>
-
-	<!--[if lt IE 9]>
-
-	<script src="/bootstrap2/media/js/excanvas.min.js"></script>
-
-	<script src="/bootstrap2/media/js/respond.min.js"></script>  
-
-	<![endif]-->                    
-
-	<script src="/bootstrap2/media/js/jquery.slimscroll.min.js" type="text/javascript"></script>
-
-	<script src="/bootstrap2/media/js/jquery.blockui.min.js" type="text/javascript"></script>  
-
-	<script src="/bootstrap2/media/js/jquery.cookie.min.js" type="text/javascript"></script>
-
-	<script src="/bootstrap2/media/js/jquery.uniform.min.js" type="text/javascript" ></script>
-
-	<!-- END CORE PLUGINS -->
-
-	<!-- BEGIN PAGE LEVEL PLUGINS -->
-
-	<script type="text/javascript" src="/bootstrap2/media/js/select2.min.js"></script>
-
-	<script type="text/javascript" src="/bootstrap2/media/js/jquery.dataTables.js"></script>
-
-	<script type="text/javascript" src="/bootstrap2/media/js/DT_bootstrap.js"></script>
-
-	<!-- END PAGE LEVEL PLUGINS -->
-
-	<!-- BEGIN PAGE LEVEL SCRIPTS -->
-
-	<script src="/bootstrap2/media/js/app.js" ></script>
-	<script src="/bootstrap2/media/js/language.js" charset='gb2312'></script>
-	<script src="/assets/js/datatables.fnReloadAjax.js" ></script> 
-	<script src="/assets/js/util.js" ></script> 
-	<script src="/bootstrap2/media/js/table-advanced.js" ></script>     
-	<script src="/bootstrap2/media/js/table-editable.js" ></script> 
 
 </head>
 
@@ -110,7 +48,7 @@
 
 <!-- BEGIN BODY -->
 
-<body class="page-header-fixed">
+<body class="page-header-fixed page-full-width">
 
 	<!-- BEGIN HEADER -->
 
@@ -124,13 +62,22 @@
 
 				<!-- BEGIN LOGO -->
 
-				<a class="brand" href="/">
+				<a class="brand" href="index.html">
 
 				<img src="/bootstrap2/media/image/logo.png" alt="logo" />
 
 				</a>
 
 				<!-- END LOGO -->
+
+				<!-- BEGIN HORIZANTAL MENU -->
+
+				<div class="navbar hor-menu hidden-phone hidden-tablet">
+
+
+				</div>
+
+				<!-- END HORIZANTAL MENU -->
 
 				<!-- BEGIN RESPONSIVE MENU TOGGLER -->
 
@@ -148,22 +95,427 @@
 
 					<!-- BEGIN NOTIFICATION DROPDOWN -->   
 
+					<li class="dropdown" id="header_notification_bar">
+
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+						<i class="icon-warning-sign"></i>
+
+						<span class="badge">6</span>
+
+						</a>
+
+						<ul class="dropdown-menu extended notification">
+
+							<li>
+
+								<p>You have 14 new notifications</p>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-success"><i class="icon-plus"></i></span>
+
+								New user registered. 
+
+								<span class="time">Just now</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-important"><i class="icon-bolt"></i></span>
+
+								Server #12 overloaded. 
+
+								<span class="time">15 mins</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-warning"><i class="icon-bell"></i></span>
+
+								Server #2 not respoding.
+
+								<span class="time">22 mins</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-info"><i class="icon-bullhorn"></i></span>
+
+								Application error.
+
+								<span class="time">40 mins</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-important"><i class="icon-bolt"></i></span>
+
+								Database overloaded 68%. 
+
+								<span class="time">2 hrs</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="label label-important"><i class="icon-bolt"></i></span>
+
+								2 user IP blocked.
+
+								<span class="time">5 hrs</span>
+
+								</a>
+
+							</li>
+
+							<li class="external">
+
+								<a href="#">See all notifications <i class="m-icon-swapright"></i></a>
+
+							</li>
+
+						</ul>
+
+					</li>
+
 					<!-- END NOTIFICATION DROPDOWN -->
 
 					<!-- BEGIN INBOX DROPDOWN -->
 
-					
+					<li class="dropdown" id="header_inbox_bar">
+
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+						<i class="icon-envelope"></i>
+
+						<span class="badge">5</span>
+
+						</a>
+
+						<ul class="dropdown-menu extended inbox">
+
+							<li>
+
+								<p>You have 12 new messages</p>
+
+							</li>
+
+							<li>
+
+								<a href="inbox.html?a=view">
+
+								<span class="photo"><img src="/bootstrap2/media/image/avatar2.jpg" alt="" /></span>
+
+								<span class="subject">
+
+								<span class="from">Lisa Wong</span>
+
+								<span class="time">Just Now</span>
+
+								</span>
+
+								<span class="message">
+
+								Vivamus sed auctor nibh congue nibh. auctor nibh
+
+								auctor nibh...
+
+								</span>  
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="inbox.html?a=view">
+
+								<span class="photo"><img src="/bootstrap2/media/image/avatar3.jpg" alt="" /></span>
+
+								<span class="subject">
+
+								<span class="from">Richard Doe</span>
+
+								<span class="time">16 mins</span>
+
+								</span>
+
+								<span class="message">
+
+								Vivamus sed congue nibh auctor nibh congue nibh. auctor nibh
+
+								auctor nibh...
+
+								</span>  
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="inbox.html?a=view">
+
+								<span class="photo"><img src="/bootstrap2/media/image/avatar1.jpg" alt="" /></span>
+
+								<span class="subject">
+
+								<span class="from">Bob Nilson</span>
+
+								<span class="time">2 hrs</span>
+
+								</span>
+
+								<span class="message">
+
+								Vivamus sed nibh auctor nibh congue nibh. auctor nibh
+
+								auctor nibh...
+
+								</span>  
+
+								</a>
+
+							</li>
+
+							<li class="external">
+
+								<a href="inbox.html">See all messages <i class="m-icon-swapright"></i></a>
+
+							</li>
+
+						</ul>
+
+					</li>
 
 					<!-- END INBOX DROPDOWN -->
 
 					<!-- BEGIN TODO DROPDOWN -->
 
-					
+					<li class="dropdown" id="header_task_bar">
+
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+						<i class="icon-tasks"></i>
+
+						<span class="badge">5</span>
+
+						</a>
+
+						<ul class="dropdown-menu extended tasks">
+
+							<li>
+
+								<p>You have 12 pending tasks</p>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="task">
+
+								<span class="desc">New release v1.2</span>
+
+								<span class="percent">30%</span>
+
+								</span>
+
+								<span class="progress progress-success ">
+
+								<span style="width: 30%;" class="bar"></span>
+
+								</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="task">
+
+								<span class="desc">Application deployment</span>
+
+								<span class="percent">65%</span>
+
+								</span>
+
+								<span class="progress progress-danger progress-striped active">
+
+								<span style="width: 65%;" class="bar"></span>
+
+								</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="task">
+
+								<span class="desc">Mobile app release</span>
+
+								<span class="percent">98%</span>
+
+								</span>
+
+								<span class="progress progress-success">
+
+								<span style="width: 98%;" class="bar"></span>
+
+								</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="task">
+
+								<span class="desc">Database migration</span>
+
+								<span class="percent">10%</span>
+
+								</span>
+
+								<span class="progress progress-warning progress-striped">
+
+								<span style="width: 10%;" class="bar"></span>
+
+								</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="task">
+
+								<span class="desc">Web server upgrade</span>
+
+								<span class="percent">58%</span>
+
+								</span>
+
+								<span class="progress progress-info">
+
+								<span style="width: 58%;" class="bar"></span>
+
+								</span>
+
+								</a>
+
+							</li>
+
+							<li>
+
+								<a href="#">
+
+								<span class="task">
+
+								<span class="desc">Mobile development</span>
+
+								<span class="percent">85%</span>
+
+								</span>
+
+								<span class="progress progress-success">
+
+								<span style="width: 85%;" class="bar"></span>
+
+								</span>
+
+								</a>
+
+							</li>
+
+							<li class="external">
+
+								<a href="#">See all tasks <i class="m-icon-swapright"></i></a>
+
+							</li>
+
+						</ul>
+
+					</li>
 
 					<!-- END TODO DROPDOWN -->
 
 					<!-- BEGIN USER LOGIN DROPDOWN -->
 
+					<li class="dropdown user">
+
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+						<img alt="" src="/bootstrap2/media/image/avatar1_small.jpg" />
+
+						<span class="username">Bob Nilson</span>
+
+						<i class="icon-angle-down"></i>
+
+						</a>
+
+						<ul class="dropdown-menu">
+
+							<li><a href="extra_profile.html"><i class="icon-user"></i> My Profile</a></li>
+
+							<li><a href="page_calendar.html"><i class="icon-calendar"></i> My Calendar</a></li>
+
+							<li><a href="inbox.html"><i class="icon-envelope"></i> My Inbox(3)</a></li>
+
+							<li><a href="#"><i class="icon-tasks"></i> My Tasks</a></li>
+
+							<li class="divider"></li>
+
+							<li><a href="extra_lock.html"><i class="icon-lock"></i> Lock Screen</a></li>
+
+							<li><a href="login.html"><i class="icon-key"></i> Log Out</a></li>
+
+						</ul>
+
+					</li>
 
 					<!-- END USER LOGIN DROPDOWN -->
 
@@ -181,12 +533,230 @@
 
 	<!-- END HEADER -->
 
-	<!-- BEGIN CONTAINER -->
+	<!-- BEGIN CONTAINER -->   
 
-	<div class="page-container row-fluid">
+	<div class="page-container row-fluid" >
 
-		@include('mwork.layouts.menu')
-		
+		<!-- BEGIN EMPTY PAGE SIDEBAR -->
+
+		<div class="page-sidebar nav-collapse collapse visible-phone visible-tablet">
+
+			<ul class="page-sidebar-menu">
+
+				<li class="visible-phone visible-tablet">
+
+					<!-- BEGIN RESPONSIVE QUICK SEARCH FORM -->
+
+					<form class="sidebar-search">
+
+						<div class="input-box">
+
+							<a href="javascript:;" class="remove"></a>
+
+							<input type="text" placeholder="Search..." />            
+
+							<input type="button" class="submit" value=" " />
+
+						</div>
+
+					</form>
+
+					<!-- END RESPONSIVE QUICK SEARCH FORM -->
+
+				</li>
+
+				<li>
+
+					<a class="active" href="index.html">
+
+					Dashboard                        
+
+					</a>
+
+				</li>
+
+				<li  class="active">
+
+					<a href="javascript:;">
+
+					Layouts
+
+					<span class="arrow open"></span>   
+
+					<span class="selected"></span>   
+
+					</a>
+
+					<ul class="sub-menu">
+
+						<li >
+
+							<a href="layout_horizontal_sidebar_menu.html">
+
+							Horzontal & Sidebar Menu                     </a>
+
+						</li>
+
+						<li class="active">
+
+							<a href="layout_horizontal_menu1.html">
+
+							Horzontal Menu 1                    </a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_horizontal_menu2.html">
+
+							Horzontal Menu 2                    </a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_promo.html">
+
+							Promo Page                    </a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_email.html">
+
+							Email Templates                     </a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_ajax.html">
+
+							Content Loading via Ajax</a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_sidebar_closed.html">
+
+							Sidebar Closed Page                    </a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_blank_page.html">
+
+							Blank Page                    </a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_boxed_page.html">Boxed Page</a>
+
+						</li>
+
+						<li >
+
+							<a href="layout_boxed_not_responsive.html">
+
+							Non-Responsive Boxed Layout                     </a>
+
+						</li>
+
+						<li>
+
+							<a href="javascript:;">
+
+							More options
+
+							<span class="arrow"></span>
+
+							</a>
+
+							<ul class="sub-menu">
+
+								<li><a href="#">Second level link</a></li>
+
+								<li>
+
+									<a href="javascript:;">More options<span class="arrow"></span></a>
+
+									<ul class="sub-menu">
+
+										<li><a href="index.html">Third level link</a></li>
+
+										<li><a href="index.html">Third level link</a></li>
+
+										<li><a href="index.html">Third level link</a></li>
+
+										<li><a href="index.html">Third level link</a></li>
+
+										<li><a href="index.html">Third level link</a></li>
+
+									</ul>
+
+								</li>
+
+								<li><a href="index.html">Second level link</a></li>
+
+								<li><a href="index.html">Second level link</a></li>
+
+								<li><a href="index.html">Second level link</a></li>
+
+							</ul>
+
+						</li>
+
+					</ul>
+
+				</li>
+
+				<li>
+
+					<a href="">Tables</a>
+
+				</li>
+
+				<li>
+
+					<a href="">Extra
+
+					<span class="arrow"></span>
+
+					</a>
+
+					<ul class="sub-menu">
+
+						<li><a href="index.html">About Us</a></li>
+
+						<li><a href="index.html">Services</a></li>
+
+						<li><a href="index.html">Pricing</a></li>
+
+						<li><a href="index.html">FAQs</a></li>
+
+						<li><a href="index.html">Gallery</a></li>
+
+						<li><a href="index.html">Registration</a></li>
+
+						<li><a href="index.html">2 Columns (Left)</a></li>
+
+						<li><a href="index.html">2 Columns (Right)</a></li>
+
+					</ul>
+
+				</li>
+
+			</ul>
+
+		</div>
+
+		<!-- END EMPTY PAGE SIDEBAR -->
+
 		<!-- BEGIN PAGE -->
 
 		<div class="page-content">
@@ -213,7 +783,7 @@
 
 			<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
 
-			<!-- BEGIN PAGE CONTAINER-->        
+			<!-- BEGIN PAGE CONTAINER-->
 
 			<div class="container-fluid">
 
@@ -311,13 +881,7 @@
 
 						</div>
 
-						<!-- END BEGIN STYLE CUSTOMIZER -->  
-
-						<!-- BEGIN PAGE TITLE & BREADCRUMB-->
-
-					
-
-						<!-- END PAGE TITLE & BREADCRUMB-->
+						<!-- END BEGIN STYLE CUSTOMIZER --> 
 
 					</div>
 
@@ -325,18 +889,18 @@
 
 				<!-- END PAGE HEADER-->
 
-				<!-- BEGIN CONTENT-->
+				<!-- BEGIN PAGE CONTENT-->
 				@yield('content')
-				<!-- END CONTENT-->
-				
+
+				<!-- END PAGE CONTENT-->
 
 			</div>
 
-			<!-- END PAGE CONTAINER-->
+			<!-- END PAGE CONTAINER--> 
 
 		</div>
 
-		<!-- END PAGE -->
+		<!-- END PAGE -->    
 
 	</div>
 
@@ -347,7 +911,9 @@
 	<div class="footer">
 
 		<div class="footer-inner">
-		Matching is All - Mapping | 麦聘咨询 &copy; 2015
+
+			Matching is All - Mapping | 麦聘咨询 &copy; 2015
+
 		</div>
 
 		<div class="footer-tools">
@@ -366,26 +932,50 @@
 
 	<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
 
-	
+	<!-- BEGIN CORE PLUGINS -->
+
+	<script src="/bootstrap2/media/js/jquery-1.10.1.min.js" type="text/javascript"></script>
+
+	<script src="/bootstrap2/media/js/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+
+	<!-- IMPORTANT! Load jquery-ui-1.10.1.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+
+	<script src="/bootstrap2/media/js/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>      
+
+	<script src="/bootstrap2/media/js/bootstrap.min.js" type="text/javascript"></script>
+
+	<!--[if lt IE 9]>
+
+	<script src="/bootstrap2/media/js/excanvas.min.js"></script>
+
+	<script src="/bootstrap2/media/js/respond.min.js"></script>  
+
+	<![endif]-->   
+
+	<script src="/bootstrap2/media/js/jquery.slimscroll.min.js" type="text/javascript"></script>
+
+	<script src="/bootstrap2/media/js/jquery.blockui.min.js" type="text/javascript"></script>  
+
+	<script src="/bootstrap2/media/js/jquery.cookie.min.js" type="text/javascript"></script>
+
+	<script src="/bootstrap2/media/js/jquery.uniform.min.js" type="text/javascript" ></script>
+
+	<!-- END CORE PLUGINS -->
+
+	<script src="/bootstrap2/media/js/app.js"></script>      
 
 	<script>
 
-		jQuery(document).ready(function() {       
+		jQuery(document).ready(function() {    
 
-			$.ajaxSetup({
-			    headers: {
-			        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-			    }
-			});
 		   App.init();
 
-		   TableAdvanced.init();
-		   //TableEditable.init();
 		});
 
 	</script>
-	@yield('menuScripts')
 	@yield('scripts')
+
+	<!-- END JAVASCRIPTS -->
 
 </body>
 
